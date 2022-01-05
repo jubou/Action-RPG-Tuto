07383de41b813d133255e07f097c664722640c16
MCruces-fz <mcsquared.fz@gmail.com>
2022-01-05 14:47:48 +0100

    Add health stats

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -5,6 +5,8 @@ const KNOCKBACK_SPEED = 120
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, KNOCKBACK_ACCEL * delta)
 	knockback = move_and_slide(knockback)
---
+++ Stats.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+export(int) var max_health = 1
+var health = max_health
