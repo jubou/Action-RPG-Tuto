ba27408ef79e2c8d9b4e257aa72a356d87f62947
MCruces-fz <mcsquared.fz@gmail.com>
2022-01-05 09:35:58 +0100

    Add hitboxes and hurtboxes to the grass animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,22 +20,24 @@ onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	animationTree.active = true
 
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 		
 		ROLL:
-			roll_state(delta)
+			roll_state()
 		
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	# Input should be in _process() function, but in Godot documentation
+	# they always use _physics_process() 
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
@@ -47,15 +49,16 @@ func move_state(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	# move_and_slide should be in _physical_process() function
 	velocity = move_and_slide(velocity)
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
-func attack_state(delta):
+func attack_state():
 	animationState.travel("Attack")
 
-func roll_state(delta):
+func roll_state():
 	pass
 
 func attack_animation_finished():
